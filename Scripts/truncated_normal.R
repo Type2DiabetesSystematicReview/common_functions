b8df28715530ebc5724c5bb2d965fd8eb854b2a5
David McAllister <david.mcallister@glasgow.ac.uk>
2024-02-23 14:15:02 +0000

    add function for deriving age mu and sigma

--- Scripts/truncated_normal.R
+++ Scripts/truncated_normal.R
@@ -26,3 +26,43 @@ var.tnorm<-function(mu,sd,lower,upper){
                                                           (pnorm(upper.std)-pnorm(lower.std)))^2)
   return(variance)
 }
+
+EstimateMuDispAge <- function (x) {
+  # Loop through trials
+  if(str_sub(x$nct_id, -1) == 1) print(x$nct_id)
+  
+  lower <- x$min_age
+  upper <- x$max_age
+  trial_mean <- x$age_m
+  trial_sd <- x$age_sd
+  trial_var <- x$age_sd^2
+  
+  ## Create grid
+  mu_x <- seq(lower, upper, 0.5)
+  sd_x <- seq(1, upper-lower, 0.5)
+  full_grid <- expand.grid(mu_x = mu_x, sd_x = sd_x)
+  
+  ## Calculate for all values of grid, is vectorised so is fast, is faster than one in truncnorm package
+  full_grid$mean_x <- mean.tnorm(full_grid$mu_x, full_grid$sd_x, lower, upper)
+  full_grid$var_x <- var.tnorm(full_grid$mu_x, full_grid$sd_x, lower, upper)
+  
+  # print(nrow(full_grid))
+  # browser()  
+  ## Identify closest values
+  full_grid <- full_grid %>%
+    as_tibble() %>%
+    mutate(mean_diff = abs(trial_mean - mean_x),
+           var_diff = abs(trial_var - var_x),
+           total_diff = mean_diff + var_diff) %>%
+    arrange(total_diff, mean_diff, var_diff)
+  ## Append original parameters
+  estimate <- full_grid %>%
+    slice(1:10) %>%
+    mutate(trial_mean = trial_mean,
+           trial_var = trial_var,
+           trial_lower = lower,
+           trial_upper = upper,
+           trial_sd = trial_sd) %>%
+    select(trial_mean, mean_x, trial_var, var_x, mu_x, sd_x, trial_sd, everything())
+  estimate 
+}
